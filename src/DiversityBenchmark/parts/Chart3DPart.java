97557fec0101a3b876e2eaa070867ed849139c79
stdo <sonthanh.do@epfl.ch>
2014-02-13 16:53:37 +0100

    update chart3d

--- src/DiversityBenchmark/parts/Chart3DPart.java
+++ src/DiversityBenchmark/parts/Chart3DPart.java
@@ -14,14 +14,16 @@ import org.eclipse.swt.widgets.Composite;
 import org.jzy3d.bridge.swt.Bridge;
 import org.jzy3d.chart.Chart;
 import org.jzy3d.chart.ChartLauncher;
-import org.jzy3d.chart.controllers.mouse.camera.CameraMouseController;
 import org.jzy3d.colors.Color;
+import org.jzy3d.maths.Coord3d;
 import org.jzy3d.plot3d.primitives.Scatter;
+import org.jzy3d.plot3d.rendering.canvas.Quality;
 
 import DiversityBenchmark.handlers.EvaluateHandler;
 import DiversityBenchmark.models.MetricModel;
 import DiversityBenchmark.utils.Constant;
 import DiversityBenchmark.utils.Constant.FACTOR;
+import DiversityBenchmark.utils.Constant.METRIC;
 import DiversityBenchmark.utils.EventConstants;
 
 public class Chart3DPart {
@@ -43,10 +45,11 @@ public class Chart3DPart {
 	public void createComposite(Composite parent) {
 		this.parent = parent;
 		initChartInfo();
-		// createTestData();
+		// // createTestData();
 		chart = createChart();
 		Bridge.adapt(parent, (java.awt.Component) chart.getCanvas());
-		chart.addController(new CameraMouseController());
+		// chart.addController(new CameraMouseController());
+		// parent.setLayout(new GridLayout());
 
 		// Bridge.adapt(parent, (Component) createChart().getCanvas());
 
@@ -54,22 +57,48 @@ public class Chart3DPart {
 
 	private Chart createChart() {
 		// TODO Auto-generated method stub
-		Chart chart = new Chart();
-		// Chart chart = new Chart(Quality.Advanced, "newt");
+		// Chart chart = new Chart();
+		Chart chart = new Chart(Quality.Advanced, "awt");
 		// Chart chart = AWTChartComponentFactory.chart(Quality.Advanced,
-		// "awt");
+		// "newt");
 		chart.getAxeLayout().setMainColor(Color.WHITE);
 		chart.getView().setBackgroundColor(Color.BLACK);
-		// chart.getScene().add(scatter);
+		scatter = testScatter();
+		chart.getScene().add(scatter);
 
 		chart.getAxeLayout().setZAxeLabel("Relevance value");
 		// ChartLauncher.openChart(chart);
 		ChartLauncher.configureControllers(chart, "Jzy3d", true, false);
 		chart.render();
+		// ChartLauncher.openChart(chart);
 
 		// Create a chart
 
 		return chart;
+
+	}
+
+	private Scatter testScatter() {
+		int size = 500000;
+		float x;
+		float y;
+		float z;
+		float a;
+
+		Coord3d[] points = new Coord3d[size];
+		Color[] colors = new Color[size];
+
+		for (int i = 0; i < size; i++) {
+			x = (float) Math.random() - 0.5f;
+			y = (float) Math.random() - 0.5f;
+			z = (float) Math.random() - 0.5f;
+			points[i] = new Coord3d(x, y, z);
+			a = 0.25f;
+			colors[i] = new Color(x, y, z, a);
+		}
+
+		Scatter scatter = new Scatter(points, colors);
+		return scatter;
 
 	}
 
@@ -91,7 +120,9 @@ public class Chart3DPart {
 			xAxisTitle = curFactorValue;
 		}
 
-		partID = EvaluateHandler.genPartID(curAlgorithm,
+		// partID = EvaluateHandler.genPartID(curAlgorithm,
+		// curFactorValue.toString());
+		partID = EvaluateHandler.genPartID(METRIC.valueOf(curAlgorithm),
 				curFactorValue.toString());
 		// partID = EvaluateHandler.genPartID(curMetric,
 		// curFactorValue.toString());
@@ -112,16 +143,26 @@ public class Chart3DPart {
 	@Inject
 	@Optional
 	void updateHandler(
-			@UIEventTopic(EventConstants.RESULT_UPDATE_UPDATED) Scatter scatter) {
+			@UIEventTopic(EventConstants.RESULT_UPDATE_UPDATED) Scatter scatter1) {
 		Object tmp = context.get(Constant.NEW3DPART);
 		assert tmp != null;
 		if (tmp instanceof Set<?>) {
 			Set<String> newparts = (Set<String>) tmp;
 			if (newparts.contains(partID)) {
-				this.scatter = scatter;
-				chart.getScene().add(scatter);
-				// this.data = data;
-				// createDataSet(data);
+				// this.scatter =
+				// chart.clear();
+				// chart.getScene().clear();
+				chart.getScene().getGraph().getAll().clear();
+				// chart.getScene().add(scatter1);
+				chart.getScene().getGraph().add(scatter1);
+				chart.render();
+
+				// this.scatter = scatter;
+				// chart.getScene().getGraph().add(drawable);
+				// testScatter();
+				// chart.getScene().add(scatter);
+				// chart.render();
+				// chart = createChart();
 			}
 		}
 	}
